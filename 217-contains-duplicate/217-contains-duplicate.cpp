73ec43a0b831e90d6fc99a9285e2bbc3fb81c888
Ahmed Salem <ahmed.salem5564@gmail.com>
2022-05-29 21:19:30 +0200

    Time: 171 ms (15.77%), Space: 51.4 MB (63.56%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {    
+        unordered_map<int,int> count;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            count[nums[i]]++;
+            if(count[nums[i]]==2){
+            return 1;
+            }
+        }
+        return 0;
+    }
+};
