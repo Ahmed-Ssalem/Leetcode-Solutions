0b95f482cb173a668663b00dd25e0ddc4c4388c3
Ahmed Salem <ahmed.salem5564@gmail.com>
2022-06-08 02:31:41 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-        if (s == "") 
-            return 0;
-        for (int i = 0, j = s.size() - 1; i < j; ++i, --j)
-            if (s[i] != s[j])
-                return 2;
-        return 1;
+        return 2 - (s == string(s.rbegin(), s.rend())) - s.empty();
     }
 };
