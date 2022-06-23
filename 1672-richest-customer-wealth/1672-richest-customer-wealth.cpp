abe0a2843e53b27cd5f750938bfb60b54afe0f51
Ahmed Salem <ahmed.salem5564@gmail.com>
2022-06-23 18:40:01 +0200

    Time: 3 ms (95.81%), Space: 7.8 MB (49.97%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int sum = 0, res = 0;
+        int res = 0;
         
-        for (int i = 0; i < accounts.size(); ++i){
-            for (int j = 0; j < accounts[i].size(); ++j){
-                sum += accounts[i][j];
-            }
-            res = max(sum, res);
-            sum = 0;
-        } 
+        for (auto &customer : accounts)
+            res = max (res, accumulate(customer.begin(), customer.end(), 0));
+        
         return res;
     }
 };
