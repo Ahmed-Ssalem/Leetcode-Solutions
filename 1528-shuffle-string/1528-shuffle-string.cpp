3cb7c675f2d9c320df0fad461411d76245d1945d
Ahmed Salem <ahmed.salem5564@gmail.com>
2022-06-27 18:14:53 +0200

    Time: 22 ms (17.87%), Space: 15.3 MB (43.13%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string res = "";
-        
-        map <int, char> m;
-        
-        for (int i = 0; i < s.length(); ++i){
-            m.insert({indices[i], s[i]});
+        string res = s;
+        for (int i = 0; i < indices.size(); ++i){
+            res[indices[i]] = s[i];
         }
-        for (auto itr = m.begin(); itr != m.end(); ++itr) {
-            res += itr->second;
-        }
-        return res;
+        return res; 
     }
 };
