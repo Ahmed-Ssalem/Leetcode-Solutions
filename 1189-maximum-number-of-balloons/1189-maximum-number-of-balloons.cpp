f42bde62a8fe8582c9531218b5a0d0a4746d6c26
Ahmed Salem <ahmed.salem5564@gmail.com>
2022-07-21 18:53:43 +0200

    Time: 7 ms (36.57%), Space: 6.7 MB (47.55%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     int maxNumberOfBalloons(string s) {
         
-        int b = 0, a = 0, l = 0, o = 0, n = 0, res = 0;
-        for (int i = 0; i < s.size(); ++i){
-            if (s[i] == 'b')
-                b++;
-            else if (s[i] == 'a')
-                a++;
-            else if (s[i] == 'l')
-                l++;
-            else if (s[i] == 'o')
-                o++;
-            else if (s[i] == 'n')
-                n++;
-        }
-        while (b >= 1 && a >= 1 && l >= 2 && o >= 2 && n >= 1){
-            b--, a--, l -= 2, o -= 2, n--, res++;
-        }
+        map <char, int> m;
+        int res = 0;
+        for (int i = 0; i < s.size(); ++i)
+            m[s[i]]++;
+        
+        while (m['b'] >= 1 && m['a'] >= 1 && m['l'] >= 2 && m['o'] >= 2 && m['n'] >= 1)
+            m['b']--, m['a']--, m['l'] -= 2, m['o'] -= 2, m['n']--, res++;
+        
         return res;
     }
 };
