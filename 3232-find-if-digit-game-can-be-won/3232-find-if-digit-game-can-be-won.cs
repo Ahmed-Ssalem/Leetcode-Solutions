a73e8d1e19c0eb81784c6bf27a22764408e8e973
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-08-07 18:39:55 +0300

    Time: 82 ms (40.87%), Space: 45.4 MB (81.2%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cs
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cs
@@ -1,16 +1,15 @@
 public class Solution {
     public bool CanAliceWin(int[] nums) {
+        
         int sumSingle = 0, sumDouble = 0;
+        
         for (int i = 0; i < nums.Length; i++){
             if(nums[i] < 10)
                 sumSingle += nums[i];
             else
                 sumDouble += nums[i];
         }
-       
-        if (sumSingle > sumDouble || sumSingle < sumDouble)
-            return true;
-        else 
-            return false;
+        
+        return sumSingle != sumDouble;
     }
 }
