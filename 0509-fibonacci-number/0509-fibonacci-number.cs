bdd2d3a3f7b20f50af80e84b8b8d0956d1817a33
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-08-13 15:43:50 +0300

    Time: 45 ms (7.68%), Space: 27 MB (34.2%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int Fib(int n) {
+        
+        return Fibonacci(n);
+    }
+    public int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+        
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+}
