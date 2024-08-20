2de037d88f9c5099596a8a2b2d871d8100f16363
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-08-20 21:44:00 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cs
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cs
@@ -1,14 +1,33 @@
 public class Solution {
     public int NumberOfPairs(int[] nums1, int[] nums2, int k) {
 
+        Dictionary<int, int> countDict = new Dictionary<int, int>();
+
+        foreach (var num in nums2){
+            var key = num * k;
+            if (!countDict.ContainsKey(key))
+                countDict[key] = 0;
+            countDict[key]++;
+        }
+
+        var goodPairs = 0;
+
+        foreach (var num in nums1)
+            foreach (var key in countDict.Keys)
+                if (num % key == 0)
+                    goodPairs++;
+            
+
+        return goodPairs;
+
+        /*
         var res = 0;
 
         for (var i = 0; i < nums1.Length; ++i)
             for (var j = 0; j < nums2.Length; ++j)
-                if (nums1[i] % (nums2[j] * k) == 0)
-                    res++;
-                
+                res += nums1[i] % (nums2[j] * k) == 0 ? 1 : 0;
 
         return res;
+        */
     }
 }
