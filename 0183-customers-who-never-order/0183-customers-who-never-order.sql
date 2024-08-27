017fa5901b545069d95cbe4767fc62be563dfbbb
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-08-27 07:24:19 +0300

    Time: 333 ms (98.87%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,8 +1,19 @@
-/* Write your T-SQL query statement below */
+/* with left join */
 
+select c.name as Customers
+from 
+    Customers c 
+left join 
+    Orders o on c.id = o.customerId
+where o.id is null
+
+/* with subquery */
+
+/*
 select name as Customers
 from Customers
 where id not in (
                     select customerId
                     from Orders
                 );
+*/
