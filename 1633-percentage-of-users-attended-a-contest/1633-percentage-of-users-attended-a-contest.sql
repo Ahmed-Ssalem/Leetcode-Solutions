f5ec562a6bfe75ae32ffae18d24a835325da51a4
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-08-29 05:10:56 +0300

    Time: 3357 ms (27.73%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+
+SELECT 
+    contest_id, 
+    ROUND( COUNT(user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY 
+    percentage DESC, 
+    contest_id ASC
