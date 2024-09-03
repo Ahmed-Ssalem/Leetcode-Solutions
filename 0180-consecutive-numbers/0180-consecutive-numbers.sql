fb709f0ce0081e744dfe9bf5378ff024e7c05797
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-09-03 12:23:54 +0300

    Time: 501 ms (92.46%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,5 +1,6 @@
 /* Write your T-SQL query statement below */
 
+/*
 SELECT DISTINCT num AS ConsecutiveNums
 FROM (
         SELECT 
@@ -16,11 +17,10 @@ WHERE
     num = next_num AND 
     next_id - id = 1 AND 
     id - prev_id = 1;
+*/
 
-/*
 SELECT DISTINCT l1.num AS ConsecutiveNums
 FROM Logs l1
 JOIN Logs l2 ON l1.id = l2.id - 1
 JOIN Logs l3 ON l1.id = l3.id - 2
 WHERE l1.num = l2.num AND l2.num = l3.num;
-*/
