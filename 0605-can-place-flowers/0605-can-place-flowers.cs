27f1a405bb7ac1f2e1c3818d3380263de685556b
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-09-08 10:34:14 +0300

    Time: 108 ms (49.97%), Space: 48.1 MB (94.58%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cs
+++ 0605-can-place-flowers/0605-can-place-flowers.cs
@@ -3,27 +3,20 @@ public class Solution {
         
         var cnt = 0;
 
-        for (int i = 0; i < flowerbed.Length; ++i)
-            if (flowerbed[i] == 0 && i != 0 && i != flowerbed.Length - 1 && flowerbed[i+1] == 0 && flowerbed[i-1] == 0){
-                cnt++;
-                flowerbed[i] = 1; 
-            }
-            else if (flowerbed[i] == 0 && i != 0 &&  i == flowerbed.Length - 1 && flowerbed[i-1] == 0){
-                cnt++;
-                flowerbed[i] = 1; 
-            }
-            else if (flowerbed[i] == 0 && i == 0 && i != flowerbed.Length - 1 && flowerbed[i+1] == 0){
-                cnt++;
-                flowerbed[i] = 1; 
-            }
-            else if (flowerbed[i] == 0 && flowerbed.Length == 1){
-                cnt++;
-                flowerbed[i] = 1; 
-            }
-
-        return cnt >= n ? true : false;
-                
+        for (int i = 0; i < flowerbed.Length; ++i){
             
-        
+            if (flowerbed[i] == 0){
+                
+                bool emptyLeftPlot = (i == 0) || (flowerbed[i-1] == 0);
+                bool emptyRightPlot = (i == flowerbed.Length - 1) || (flowerbed[i+1] == 0);
+            
+                if (emptyLeftPlot && emptyRightPlot){
+                    cnt++;
+                    flowerbed[i] = 1; 
+                }
+            }
+        }
+
+        return cnt >= n;   
     }
 }
