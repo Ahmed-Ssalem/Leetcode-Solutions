d354b7b0c81bb99b31b0dab18e08389bad243a95
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-09-09 10:09:44 +0300

    Time: 334 ms (96.23%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+
+
+
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree
