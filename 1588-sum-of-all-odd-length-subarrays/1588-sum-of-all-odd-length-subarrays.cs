6cde5a9d554894deb14e4e55065a2880ade6bc3a
Ahmed Salem <ahmed.salem5564@gmail.com>
2024-09-29 15:45:29 +0300

    Time: 54 ms (90.72%), Space: 40.2 MB (94.85%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cs
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cs
@@ -1,9 +1,20 @@
 public class Solution {
     public int SumOddLengthSubarrays(int[] arr) {
         
+        // O(n) solution
+
+        int res = 0, n = arr.Length;
+        
+        for (int i = 0; i < n; ++i)
+            res += ((n - i) * (i + 1) + 1) / 2 * arr[i];
+        
+        return res;
+
+        // with prefix sum O(n^2) solution
+
+        /*
         int len = arr.Length, res = 0;
         var prefix = new int[len+1];
-        prefix[0] = arr[0];
 
         for (int i = 0; i < len; ++i)
         {
@@ -16,11 +27,10 @@ public class Solution {
             int right = i;
 
             for (int left = i - 3; left >= 0; left -= 2)
-                res += prefix[right] - prefix[left];    
-            
+                res += prefix[right] - prefix[left];                
         }
 
-
-        return res;        
+        return res;  
+        */      
     }
 }
