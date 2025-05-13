5c5c0a7eed054faccea54602dda8b2df23d955cb
Ahmed Salem <ahmed.salem5564@gmail.com>
2025-05-14 00:30:53 +0300

    Time: 2 ms (74.24%), Space: 68.7 MB (14.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,23 +1,67 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
+
+        int n = nums.Length;
+        int [] prefix = new int[n];
+        int [] postfix = new int[n];
+        int [] res = new int[n];
+
+        prefix[0] = nums[0];
+        postfix[n-1] = nums[n-1];
+
+        for (int i = 1; i < n; ++i) 
+            prefix[i] = nums[i] * prefix[i-1];
         
+        for (int i = n - 2; i > 0; --i){
+            postfix[i] = nums[i] * postfix[i+1];
+        }
+
+        for (int i = 0; i < n; ++i){
+            int left = i > 0 ? prefix[i-1] : 1;
+            int right= i < n - 1 ? postfix[i+1] : 1;
+            res[i] = left * right; 
+        }
+
+        return res;
+        
+        // int[] res = new int[nums.Length];
+
+        // int mul = 1;
+        // for (int i = 0; i < nums.Length; ++i){
+        //     res[i] = mul;
+        //     mul *= nums[i];
+        // }
+
+        // mul = 1;
+        // for (int i = nums.Length - 1; i >= 0; --i){
+        //     res[i] *= mul;
+        //     mul *= nums[i];
+        // }
+        
+        // return res;
+
+        /*
         int[] res = new int[nums.Length];
+        Array.Fill(res, 0);
+        int zeroes = 0, product = 1;
 
         for (int i = 0; i < nums.Length; ++i){
-            
-            int leftMul = 1, rightMul = 1;
-            // left nums
-            for (int j = i + 1; j < nums.Length; ++j)
-                leftMul *= nums[j];
-
-            // right nums
-            for (int j = 0; j < i; ++j)
-                rightMul *= nums[j];
-
-            res[i] = rightMul * leftMul;
-            
+           if (nums[i] == 0)
+                zeroes++;
+            else
+                product *= nums[i];        
         }
+
+        if (zeroes == 1 )
+            for (int i = 0; i < nums.Length; ++i)
+                res[i] = nums[i] == 0 ? product : 0;
+
+        else if (zeroes == 0)
+            for (int i = 0; i < nums.Length; ++i)
+                res[i] = product / nums[i];
+        
             
         return res;
+        */
     }
 }
