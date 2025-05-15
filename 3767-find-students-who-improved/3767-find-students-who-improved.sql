c193479fb0ac1e61af00243e675f3b919a37955a
Ahmed Salem <ahmed.salem5564@gmail.com>
2025-05-15 14:29:33 +0300

    Time: 305 ms (64.72%), Space: 0B (100%) - LeetHub

---
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+select s1.student_id, s1.subject, s1.first_score, s2.latest_score 
+from
+(select distinct
+    student_id, 
+    subject, 
+    FIRST_VALUE(score) OVER (
+        PARTITION BY student_id, subject
+        ORDER BY exam_date asc
+    ) AS first_score
+from scores) as s1
+join 
+(select distinct
+    student_id, 
+    subject, 
+    FIRST_VALUE(score) OVER (
+        PARTITION BY student_id, subject
+        ORDER BY exam_date desc
+    ) AS latest_score
+from scores) as s2
+on s1.student_id = s2.student_id and s1.subject = s2.subject and s2.latest_score > s1.first_score
