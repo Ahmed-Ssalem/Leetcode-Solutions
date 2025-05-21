4aae41518c40b2d8c94bd5b9acb89b28c5c59d57
Ahmed Salem <ahmed.salem5564@gmail.com>
2025-05-21 11:13:06 +0300

    Time: 0 ms (100%), Space: 8.6 MB (54%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0){
+            
+            int rem = x % 10;
+
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && rem > 7)) 
+                return 0;
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && rem < -8)) 
+                return 0;
+
+            res = (res * 10) + rem;
+            x /= 10;
+        }
+        return res;
+    } 
+};
