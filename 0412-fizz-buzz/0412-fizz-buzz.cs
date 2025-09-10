15e1faa929e544af14eef049368e21e9a694eefb
Ahmed Salem <ahmed.salem5564@gmail.com>
2025-09-10 09:59:48 +0300

    Time: 7 ms (41.51%), Space: 68.1 MB (71.52%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        
+        IList<string> res = new List<string>();
+
+        for (int i = 1; i <= n; ++i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                res.Add("FizzBuzz");
+            else if (i % 3 == 0)
+                res.Add("Fizz");
+            else if (i % 5 == 0)
+                res.Add("Buzz");
+            else 
+                res.Add(i.ToString());
+        }
+        return res;
+    }
+}
