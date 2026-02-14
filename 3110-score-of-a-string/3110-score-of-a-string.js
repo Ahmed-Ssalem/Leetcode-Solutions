5c509e55cb556f5587e6645e5a84a8c48c382cbe
Ahmed Salem <ahmed.salem5564@gmail.com>
2026-02-14 15:06:04 +0200

    Time: 0 ms (100%), Space: 55.9 MB (44.89%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    
+    let res = 0;
+
+    for (let i = 0; i < s.length - 1; ++i)
+        res += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1))
+
+    return res;
+};
